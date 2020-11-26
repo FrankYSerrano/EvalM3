fd932e45d83ebbfda7dbfac1bdfb31ac2a36faa3
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-26 00:03:33 -0300

    el menu

--- funciones/menu.py
+++ funciones/menu.py
@@ -6,12 +6,18 @@ def menu():
     control = True
     while control: 
         os.system("CLS")
-        print("******* MENU *******")
-        print("* Opcion 1    -> 1 *")
-        print("* Opcion 2    -> 2 *")
-        print("* Opcion 3    -> 3 *")
-        print("* Opcion 4    -> 4 *")
-        print("********************")
+        print("***************** MENU *****************")
+        print("*                                      *")
+        print("* Recepcion de Flores en Bodega   -> 1 *") # Fernando
+        print("* Generar Disenos de Ramos        -> 2 *") # Frank
+        print("* Seleccionar (Ordenar) Ramos     -> 3 *") # Guido
+        print("* Reporte de inventario de Bodega -> 4 *") # Cristian
+        print("* Reporte de Ramos pendientes     -> 5 *") # Cristian
+        print("* Reporte de Ramos entregados     -> 6 *") # Cristian
+        print("* Evaluar Ramos Procesables       -> 7 *") # Cristian
+        print("*       S   A   L   I   R         -> X *")
+        print("*                                      *")
+        print("****************************************")
         opcion = input("Introduzca su opcion: ")
         if opcion in opciones_posibles:
             control = False
