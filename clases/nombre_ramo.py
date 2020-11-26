f64176bbf8096769d6b01030b414280dcd9218e1
FrankYSerrano <FRANKYSERRANO@GMAIL.COM>
2020-11-25 21:41:09 -0300

    9pm

---
+++ clases/nombre_ramo.py
@@ -0,0 +1,14 @@
+#CLASE
+class nombre_ramo: 
+#ATRIBUTOS
+    
+#METODOS:
+    def __init__ (self, letra):
+        self.letra = letra
+        print("se ha añadido un nombre de ramo")  
+          
+if __name__ == "__main__":
+    nombre_ramo("A")
+    nombre_ramo("B")
+
+           
---
+++ clases/tamano_ramo.py
@@ -0,0 +1,14 @@
+#CLASE
+class tamano_ramo: 
+#ATRIBUTOS
+    
+#METODOS:
+    def __init__ (self, tamano):
+        self.tamano = tamano
+        print("se ha añadido un tamano de ramo")
+                  
+if __name__ == "__main__":
+    tamano_ramo("L")
+    tamano_ramo("S")
+
+           
