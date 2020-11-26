bb34322e5ece66cbdf6c86b7e22f87cc21ac49f2
BlueSkyne <dressius@gmail.com>
2020-11-25 23:41:14 -0300

    funcionalidades

--- clases/diseno_ramo.py
+++ clases/diseno_ramo.py
@@ -1,20 +1,23 @@
 
-
 #CLASE
-class Clase03 (flor):
+class diseno_ramo ():
 #ATRIBUTOS
     
+   
 #METODOS:
-    def __init__ (self, nombre_ramo, tamano_ramo):
-        self.nombre_ramo = arg1
-        self.atr2 = arg2
-        self.atr3 = arg3
-        self.atr4 = arg4
-        Clase01.__init__(self, arg1, arg2, arg3, arg4)
-        Clase02.__init__(self, arg1, arg2, arg3, arg4)
-        print("Cree instancia de clase 03")
+    '''
+    def __init__ (self, nombre_ramo,tamano,cantidad,letra):
+        super().__init__(letra,tamano)
+        self.letra = letra
+        self.nombre_ramo = nombre_ramo
+        self.tamano = tamano_ramo
+        return nombre_ramo,tamano,cantidad,letra
 
-    def met1 (self, arg1):
+        print("este es el tamaño",tamano_ramo)
+    '''    
+    def __init__ (self, diseno):
+        self.diseno = diseno
+
         print("Clase 03 Metodo 1")
 
     def met2 (self, arg1):
@@ -22,3 +25,7 @@ class Clase03 (flor):
 
     def met3 (self, arg1):
         print("Clase 03 Metodo 3")
+
+if __name__ == "__main__":
+    
+    pass
